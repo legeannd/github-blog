04b41e2365b5c5f4986093456e3259663f551342
Gean Lucas <gean.lfpsa@gmail.com>
2023-05-29 11:55:35 -0300

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <h1 className="text-3xl font-bold underline bg-red-800 text-white">
+      Hello World
+    </h1>
+  )
 }
 
 export default App
